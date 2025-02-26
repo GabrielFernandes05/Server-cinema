2af7bb654748062ad0372bc2ae66922e0ff0255e
Gabriel Fernandes de Freitas Moreira Duarte de Lima <gabrielfernandes2005@gmail.com>
2025-02-26 17:26:51 -0300

    feature/cors

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupUserRoutes(r *gin.Engine, db *gorm.DB) {
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	userGroup := r.Group("/users")
 	{
 		userGroup.GET("/getusers", func(c *gin.Context) {
